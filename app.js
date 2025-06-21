21749b2502b7ed5d3cf0f05cd05ff7c2f1caa583
WONG SIN YAW <ICT_3337922K@connect.np.edu.sg>
2025-06-21 10:38:38 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json()); // parse JSON body
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+let goals = [
+  { id: 1, title: "Learn Express" },
+  { id: 2, title: "Build a CRUD API" },
+];
+
+app.get("/api/goals", (req, res) => {
+  res.json(goals);
+});
