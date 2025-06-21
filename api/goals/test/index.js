463ad8f63768e69007a423dedb385c84c2235ddd
DipSinYaw <ICT_3337922K@connect.np.edu.sg>
2025-06-21 12:45:19 +0800

    add

--- api/goals/test/index.js
+++ api/goals/test/index.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from Serverless Function!" });
+// export default function handler(req, res) {
+//   res.status(200).json({ message: "Hello from Serverless Function!" });
+// }
+module.exports= (req, res) => {
+if (req.method=== 'GET') {
+res.status(200).json(['goal1', 'goal2']);
+} else if (req.method=== 'POST') {
+res.status(201).json({ message: 'Created' });
 }
+};
