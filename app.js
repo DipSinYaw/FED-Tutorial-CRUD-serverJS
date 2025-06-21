25235849a1215e934a6243a043838a68f268e85c
DipSinYaw <ICT_3337922K@connect.np.edu.sg>
2025-06-21 11:03:59 +0800

    add

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json()); // parse JSON body
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
 
 let goals = [
   { id: 1, title: "Learn Express" },
@@ -15,4 +12,30 @@ app.get("/api/goals", (req, res) => {
   res.json(goals);
 });
 
+app.post("/goals", (req, res) => {
+  let newGoal = { id: Date.now(), ...req.body };
+  goals.push(newGoal);
+  res.status(201).json(newGoal);
+});
 
+app.put("/goals/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let index = goals.findIndex((g) => g.id === id);
+  if (index !== -1) {
+    goals[index] = { ...goals[index], ...req.body };
+    res.json(goals[index]);
+  } else {
+    res.status(404).json({ error: "Goal not found" });
+  }
+});
+
+app.delete("/goals/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  goals = goals.filter((g) => g.id !== id);
+  res.json({ message: "Deleted successfully" });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
