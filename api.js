f6b3d63ec31d41f8e7de68537870667a73834c81
DipSinYaw <ICT_3337922K@connect.np.edu.sg>
2025-06-21 14:13:19 +0800

    test if get method

--- api.js
+++ api.js
@@ -1,9 +1,51 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js API!');
+app.get("/", (req, res) => {
+  if (req.method === "GET") {
+    res.send("Hello from Node.js API!");
+    // res.status(200).json(['goal1', 'goal2']);
+  }
+  //     else if (req.method === 'POST') {
+  // res.status(201).json({ message: 'Created' });
+  // }
 });
+
+let goals = [
+  { id: 1, title: "Learn Express" },
+  { id: 2, title: "Build a CRUD API" },
+];
+
+app.get("/api/goals", (req, res) => {
+  res.json(goals);
+});
+
+app.post("/api/goals", (req, res) => {
+  let newGoal = { id: Date.now(), ...req.body };
+  goals.push(newGoal);
+  res.status(201).json(newGoal);
+});
+
+app.put("/api/goals/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let index = goals.findIndex((g) => g.id === id);
+  if (index !== -1) {
+    goals[index] = { ...goals[index], ...req.body };
+    res.json(goals[index]);
+  } else {
+    res.status(404).json({ error: "Goal not found" });
+  }
+});
+
+app.delete("/api/goals/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  goals = goals.filter((g) => g.id !== id);
+  res.json({ message: "Deleted successfully" });
+});
+
+export default function handler(req, res) {
+  res.status(200).json({ message: "Hello from Serverless Function!" });
+}
