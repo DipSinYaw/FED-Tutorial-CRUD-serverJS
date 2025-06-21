e6454a06b4420eef1f7715ed18713cc716eaf495
DipSinYaw <ICT_3337922K@connect.np.edu.sg>
2025-06-21 14:47:15 +0800

    try again

--- api.js
+++ api.js
@@ -4,20 +4,15 @@ const app = express();
 
 // app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js API!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello from Node.js API!');
+// });
 
 
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
-
 
 let goals = [
   { id: 1, title: "Learn Express" },
@@ -49,4 +44,8 @@ app.delete("/api/goals/:id", (req, res) => {
   let id = parseInt(req.params.id);
   goals = goals.filter((g) => g.id !== id);
   res.json({ message: "Deleted successfully" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
